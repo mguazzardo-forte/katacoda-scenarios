e5afda3eb334c7103495b1a3f19dc09dc34ceddc
root <root@qqmelo2c.mylabserver.com>
2020-03-04 03:18:02 +0000

    ansible docker

---
+++ ansible-docker/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+__create_user() {
+# Create a user to SSH into as.
+useradd devops 
+
+echo "root:master" | chpasswd
+echo "devops:master" | chpasswd
+
+}
+
+# Call all functions
+__create_user
