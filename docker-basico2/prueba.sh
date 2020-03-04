6d6a79023b47d3ff5bdc6be773d277ac41b6323a
mguazzardo <mguazzardo76@gmail.com>
2020-03-03 21:39:59 -0300

    Update prueba.sh

--- docker-basico2/prueba.sh
+++ docker-basico2/prueba.sh
@@ -1,1 +1,18 @@
-echo  "hola mundo" && mkdir prueba
+mkdir /root/DockerSSH
+cat << EOF >/root/DockerSSH/start.sh
+#!/bin/bash
+
+__create_user() {
+# Create a user to SSH into as.
+useradd user
+SSH_USERPASS=Espejito.76
+echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
+echo ssh user password: $SSH_USERPASS
+
+echo "root:Espejito.76" | chpasswd
+
+}
+
+# Call all functions
+__create_user
+EOF
