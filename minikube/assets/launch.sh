d985d65a3cc41bf56e77db101da137d1113d9531
mguazzardo <mguazzardo76@gmail.com>
2021-09-14 07:46:57 -0300

    Update launch.sh

--- minikube/assets/launch.sh
+++ minikube/assets/launch.sh
@@ -4,5 +4,6 @@ while [ `docker ps | wc -l` -eq 1 ]
 do
   sleep 1
 done
+wget https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens && chmod +x kubens && mv kubens /usr/local/bin/
 
 echo "Kubernetes Start y corriendo"
