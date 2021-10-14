bd7167b142256565c7abb18b0b0ca53cdae3fd2d
mguazzardo <mguazzardo76@gmail.com>
2021-10-14 13:11:16 -0300

    Update background.sh

--- dind/background.sh
+++ dind/background.sh
@@ -1,1 +1,1 @@
-docker run -d --name oc-cluster-up -it -p 9000:9000 -v /tmp/:/tmp/ -v /var/run/docker.sock:/var/run/docker.sock gustavonalle/oc-cluster-up
+docker run -e "NODES=3" -d --name oc-cluster-up -it -p 9000:9000 -v /tmp/:/tmp/ -v /var/run/docker.sock:/var/run/docker.sock gustavonalle/oc-cluster-up
