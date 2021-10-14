58fa817f886581340722af56ad8fb62ddb09982c
Your Name <you@example.com>
2021-10-14 13:25:32 -0300

    dind-openshift

---
+++ dind-openshift/foreground.sh
@@ -0,0 +1,1 @@
+echo "docker run -e "NODES=3" -d --name oc-cluster-up -it -p 9000:9000 -v /tmp/:/tmp/ -v /var/run/docker.sock:/var/run/docker.sock gustavonalle/oc-cluster-up" > /root/correr-cluster.sh
