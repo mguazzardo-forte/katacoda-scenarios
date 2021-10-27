746619a749296a08a2cad109154841c02aefafcb
Your Name <you@example.com>
2021-10-27 08:10:38 -0300

    subiendo gitops

---
+++ gitops01/set-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+## Wait until the file exists before running it
+until [ -f resources/scripts/argocd-postinstall.sh ]
+do
+    sleep 5
+done
+
+#
+## Launch the script that sets up ArgoCD with customizations
+bash resources/scripts/argocd-postinstall.sh
+##
+##
+
+git clone https://github.com/mguazzardo/pipelinefinal
