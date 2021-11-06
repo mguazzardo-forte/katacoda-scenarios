e45598e7bdc1b31113ab5502a920568afb45a2d1
Your Name <you@example.com>
2021-11-05 22:38:14 -0300

    cambio port

--- simple-node/index.js
+++ simple-node/index.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
   res.end("<h1>Hello World from OpenShift NodeJS App</h1>");
 });
 
-server.listen(8000, () => {
+server.listen(8080, () => {
   console.log("Listening to requests on Port 8000");
 });
